1098faf68b7cd45be96212c2c7647f6688ff9daf
Mallarden <antonanderson.com@gmail.com>
2018-08-16 01:47:16 +0200

    new imgs and some transitions

--- src/App.js
+++ src/App.js
@@ -18,11 +18,12 @@ class App extends Component {
             <Col xs={12}>
               <Col xs={10} className="front-img-col" style={{borderTop:'4px solid white', borderBottom:'4px solid white'}}>
               <div className="front-text-container">
-                <div className="front-img-text-left fadeInUp"><p>Visual brilliance</p></div>  
-                <div className="front-img-text-right fadeInDown"><p>Design</p></div> 
+                <div className="front-img-text-left fadeInUp"><p>visual brilliance</p></div> 
+                <div className="front-img-text-right fadeInDown"><p>design</p></div>  
+                <div className="front-img-text-right-down fadeInUp"><p>rethinking</p></div> 
               </div>            
                 <img src={require('./img/test1.jpg')} className="front-img" />   
-                <div className="circle-test"></div>
+                <div className="circle-test rotateInDownRight"></div>
               </Col>
               <Col xs={1} className="col nopadding" style={{height:'100%'}}> 
                 <div className="stripe"></div>
@@ -40,17 +41,17 @@ class App extends Component {
             <Row className="page-grid">
                 <Col sm={4} className="">
                   <Col sm={11} className="item">  
-                  <img src={require('./img/test1.jpg')} className="grid-img" />   
+                  <img src={require('./img/test4.jpg')} className="grid-img" />   
+                  </Col>
+                </Col>
+                <Col sm={4} className="">
+                  <Col sm={11} className="item">
+                  <img src={require('./img/test3.jpg')} className="grid-img" />   
                   </Col>
                 </Col>
                 <Col sm={4} className="">
                   <Col sm={11} className="item">
                   <img src={require('./img/test2.jpg')} className="grid-img" />   
-                  </Col>
-                </Col>
-                <Col sm={4} className="">
-                  <Col sm={11} className="item">
-                  <img src={require('./img/test1.jpg')} className="grid-img" />   
                   </Col>
                 </Col>
             </Row>
